ac517fda6923b1e8edb00d08ba1057c8a2c8e16b
marynad <mskameneva@gmail.com>
2025-03-07 22:52:11 +0200

    add contact form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
 import initialContacts from "../contacts.json";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ContactForm from "./ContactForm/ContactForm";
 import SearchBox from "./SearchBox/SearchBox";
 import ContactList from "./ContactList/ContactList";
 
 export default function App() {
-  const [contacts, setContacts] = useState(initialContacts);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+  });
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
 
   const deleteContact = (contactId) => {
     setContacts((prevContacts) => {
@@ -21,7 +32,7 @@ export default function App() {
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm />
+      <ContactForm onAdd={addContact} />
       <SearchBox value={filter} onFilter={setFilter} />
       <ContactList contacts={visibleContacts} onDelete={deleteContact} />
     </div>
